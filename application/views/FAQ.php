b921d4d5411855e774aa7f0357596c6a5c8d2fe9
sukherbyviru5 <poetraarromadhon56@gmail.com>
2025-09-04 14:43:27 +0700

    fix : domain

--- application/views/FAQ.php
+++ application/views/FAQ.php
@@ -26,20 +26,20 @@
 </p>
 <li>
 <b>Barang apa saja yang dapat dikirim melalui Trip Cargo?</b><br>
-<p>Semua jenis barang seperti paket, dokumen dan cargo berat dapat kami kirimkan kecuali barang-barang yang berbahaya dan dilarang seperti: binatang, emas, perak, uang, bahan peledak dan barang-barang mudah terbakar, senjata api dan amunisi, petasan, perhiasan logam mulia dan batu berharga, narkotika, alkohol, makanan dan minuman yang tidak disahkan oleh Departemen Kesehatan RI dan barang-barang ilegal lainnya. Untuk selengkapnya silakan baca di halaman <a href="https://tripcargo.test/Terms">Syarat & Ketentuan</a>.</li><br>
+<p>Semua jenis barang seperti paket, dokumen dan cargo berat dapat kami kirimkan kecuali barang-barang yang berbahaya dan dilarang seperti: binatang, emas, perak, uang, bahan peledak dan barang-barang mudah terbakar, senjata api dan amunisi, petasan, perhiasan logam mulia dan batu berharga, narkotika, alkohol, makanan dan minuman yang tidak disahkan oleh Departemen Kesehatan RI dan barang-barang ilegal lainnya. Untuk selengkapnya silakan baca di halaman <a href="https://tripcargoid.com/Terms">Syarat & Ketentuan</a>.</li><br>
 </p>
 <li>
 <b>Kota/daerah mana saja yang dapat dijangkau oleh layanan Trip Cargo?</b></br>
 <p>Trip Cargo dapat menjangkau kota-kota besar di seluruh Indonesia.</p></li>
 <li>
 <b>Di mana kantor cabang dan perwakilan Trip Cargo?</b><br>
-<p>Sampai saat ini Trip Cargo memiliki cabang Jawa, Bali, Sumatera,Kalimantan, Makassar perwakilan yang tersebar di seluruh Indonesia, dan akan terus bertambah. Silakan cek di daftar kantor kami <a href="https://tripcargo.test/hubungi_kami">di sini.</a></p></li><br>
+<p>Sampai saat ini Trip Cargo memiliki cabang Jawa, Bali, Sumatera,Kalimantan, Makassar perwakilan yang tersebar di seluruh Indonesia, dan akan terus bertambah. Silakan cek di daftar kantor kami <a href="https://tripcargoid.com/hubungi_kami">di sini.</a></p></li><br>
 <li>
 <b>Berapa lama waktu yang dibutuhkan untuk mengirim barang?</b><br>
-<p>Waktu yang dibutuhkan untuk pengiriman sangat tergantung (Kota asal) dan tujuan akhir dari paket atau kondisi lain yang menyebabkan pengiriman tertunda dari estimasi dibawah berikut ini tertera pada tampilan <a href="https://tripcargo.test/web/cektarif">di sini.</p></a></li><br>
+<p>Waktu yang dibutuhkan untuk pengiriman sangat tergantung (Kota asal) dan tujuan akhir dari paket atau kondisi lain yang menyebabkan pengiriman tertunda dari estimasi dibawah berikut ini tertera pada tampilan <a href="https://tripcargoid.com/web/cektarif">di sini.</p></a></li><br>
 <li>
 <b>Bagaimana cara mengetahui status pengiriman barang?</b><br>
-<p>Anda bisa mengecek status pengiriman barang dengan memasukkan nomor resi pada layananan track barang <a href="https://tripcargo.test/tracking">di sini.</p></a></li><br>
+<p>Anda bisa mengecek status pengiriman barang dengan memasukkan nomor resi pada layananan track barang <a href="https://tripcargoid.com/tracking">di sini.</p></a></li><br>
 <li>
 <b>Saya hendak mengirimkan beberapa paket sekaligus, bisakah Trip Cargo mengambil paket tersebut di rumah atau toko saya?</b><br>
 <p>Bisa. Untuk pengiriman di atas 150 kg, kami ambil di rumah/toko Anda. Waktu pengambilan pun bisa fleksibel.</p></li>
